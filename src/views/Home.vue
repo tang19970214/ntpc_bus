167ad9ebbbd01644474a37521bf52e4aa7c882dc
coderken <jk0910948659@gmail.com>
2021-06-02 00:32:21 +0800

    sort out function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,14 @@
       :busList="busList"
       @getBusInfo="getBusInfo"
       @initMap="initMap()"
+      @getBusOrder="getBusOrder"
     />
     <div id="map" ref="map"></div>
   </div>
 </template>
 
 <script>
-import BusListForm from "../components/BusListForm.vue";
+import BusListForm from "../components/BusListForm.vue"
 
 export default {
   name: "Home",
@@ -20,12 +21,22 @@ export default {
       busList: [],
       busMarkList: [],
       map: document.getElementById("map"),
-    };
+    }
   },
   methods: {
+    //勾選巴士後更新 busMarkList
+    getBusOrder(order) {
+      if (order.length > 0) {
+        this.busMarkList = order
+      } else {
+        this.busMarkList = this.$store.state.busMarkList
+      }
+    },
+
+    //定位巴士，顯示 dialog
     getBusInfo(row) {
-      let getMark = this.busMarkList.filter((res) => res.BusID == row.carId)[0];
-      this.map.panTo({ lat: Number(getMark.Lat), lng: Number(getMark.Lon) });
+      let getMark = this.busMarkList.filter((res) => res.BusID == row.carId)[0]
+      this.map.panTo({ lat: Number(getMark.Lat), lng: Number(getMark.Lon) })
     },
     initMap() {
       this.map = new google.maps.Map(document.getElementById("map"), {
@@ -49,15 +60,14 @@ export default {
         // terrain 顯示基於地形信息的物理地圖。
         mapTypeId: "roadmap",
         // 需要再前往修改，https://mapstyle.withgoogle.com/
-      });
-
-      let marker = this.setMaker();
+      })
+      let marker = this.setMaker()
     },
-    setMaker(lat, lon) {
+    setMaker() {
       this.busMarkList.forEach((location) => {
         const busInfo = this.busList.filter(
           (res) => res.BusID == location.carId
-        )[0];
+        )[0]
         const marker = new google.maps.Marker({
           //原始中心點
           position: {
@@ -66,48 +76,54 @@ export default {
           },
           icon: require("@/assets/images/busIcon.png"),
           map: this.map,
-        });
+        })
         // 透過 InfoWindow 物件建構子建立新訊息視窗
-        const infowindow = new google.maps.InfoWindow({
-          // 設定想要顯示的內容
-          content:
-            `
+        this.getDialogInfo(busInfo)
+        this.openDialogInfo(marker)
+      })
+    },
+    getDialogInfo(busInfo) {
+      const infowindow = new google.maps.InfoWindow({
+        // 設定想要顯示的內容
+        content:
+          `
           <div class="markerPopover">
             <p>駕員：` +
-            busInfo.driverName +
-            `</p>
+          busInfo.driverName +
+          `</p>
             <p>聯絡電話：` +
-            busInfo.driverPhone +
-            `</p>
+          busInfo.driverPhone +
+          `</p>
             <p>任務說明：` +
-            busInfo.dutyDesc +
-            `</p>
+          busInfo.dutyDesc +
+          `</p>
             <p>車速：` +
-            location.Speed +
-            `</p>
+          location.Speed +
+          `</p>
             <p>車輛：` +
-            busInfo.carNo +
-            `</p>
+          busInfo.carNo +
+          `</p>
           </div>
         `,
-          // 設定訊息視窗最大寬度
-          maxWidth: 200,
-        });
-        // 在地標上監聽點擊事件
-        marker.addListener("click", () => {
-          // 指定在哪個地圖和地標上開啟訊息視窗
-          infowindow.open(this.map, marker);
-        });
-      });
+        // 設定訊息視窗最大寬度
+        maxWidth: 200,
+      })
+    },
+    openDialogInfo(marker) {
+      // 在地標上監聽點擊事件
+      marker.addListener("click", () => {
+        // 指定在哪個地圖和地標上開啟訊息視窗
+        infowindow.open(this.map, marker)
+      })
     },
   },
   mounted() {
-    this.busList = this.$store.state.busList;
-    this.busMarkList = this.$store.state.busMarkList;
-    this.initMap();
-    this.setMaker();
+    this.busList = this.$store.state.busList
+    this.busMarkList = this.$store.state.busMarkList
+    this.initMap()
+    this.setMaker()
   },
-};
+}
 </script>
 
 <style lang="scss">
