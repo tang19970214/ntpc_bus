e6bf9e31197741d265295b637ad56bf312098402
coderken <jk0910948659@gmail.com>
2021-06-01 17:40:37 +0800

    get order list new arry

--- src/components/BusListForm.vue
+++ src/components/BusListForm.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="busListFormPage">
-    <div class="busListFormPage__tollBar">
-      <el-button type="warning" plain @click="initMap()">重新載入 google map</el-button>
-      <el-button type="primary" plain @click="showTollBar = !showTollBar">{{showTollBar ? "隱藏" : "顯示"}}</el-button>
-    </div>
-    <div class="busListFormPage__table" v-if="showTollBar">
-      <el-table :data="busList" height="300" stripe style="width: 100%" @row-click="getBusInfo">
-        <el-table-column label="編號" prop="id" width="100px"></el-table-column>
-        <el-table-column label="車輛" prop="carNo" width="130px"></el-table-column>
-        <el-table-column label="駕員" prop="driverName" width="60px"></el-table-column>
-        <el-table-column label="所屬" prop="companyStation" width="180px"></el-table-column>
-        <el-table-column label="任務備註" prop="dutyDesc" width="150px"></el-table-column>
-        <el-table-column label="狀態" prop="dutyStatus" width="100px"></el-table-column>
-      </el-table>
+    <el-tabs type="border-card" class="busListFormPage__tollBar">
+      <el-tab-pane label="任務管理">
+        <el-button type="warning" plain @click="initMap()"
+          >重新載入 google map</el-button
+        >
+        <div class="busListFormPage__table">
+          <el-table
+            :data="busList"
+            height="300"
+            stripe
+            style="width: 100%"
+            @row-click="getBusInfo"
+            @selection-change="selectionChange"
+            :show-overflow-tooltip="true"
+            :stripe="false"
+          >
+            <el-table-column type="selection" width="55"></el-table-column>
+            <el-table-column
+              label="編號"
+              prop="id"
+              width="100px"
+            ></el-table-column>
+            <el-table-column
+              label="車輛"
+              prop="carNo"
+              width="130px"
+            ></el-table-column>
+            <el-table-column
+              label="駕駛員"
+              prop="driverName"
+              width="80px"
+            ></el-table-column>
+            <el-table-column
+              label="所屬"
+              prop="companyStation"
+              width="180px"
+            ></el-table-column>
+            <el-table-column
+              label="任務備註"
+              prop="dutyDesc"
+              width="150px"
+            ></el-table-column>
+            <el-table-column
+              label="狀態"
+              prop="dutyStatus"
+              width="100px"
+            ></el-table-column>
+          </el-table>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="地圖監控"></el-tab-pane>
+    </el-tabs>
+    <!-- <div class="busListFormPage__tollBar">
+      <el-button type="warning" plain @click="initMap()"
+        >重新載入 google map</el-button
+      > -->
+      <!-- <el-button type="primary" plain @click="showTollBar = !showTollBar">{{
+        showTollBar ? "隱藏" : "顯示"
+      }}</el-button> -->
     </div>
   </div>
 </template>
@@ -28,29 +74,42 @@ export default {
   data() {
     return {
       showTollBar: true,
-    };
+      busOrder: [], //替換成busList: , busMarkList: length>0，才換
+    }
   },
   methods: {
     getBusInfo(row) {
-      this.$emit("getBusInfo", row);
+      console.log("getBusInfo",row);
+      this.$emit("getBusInfo", row)
     },
     initMap() {
-      this.$emit("initMap");
+      this.$emit("initMap")
+    },
+    selectionChange(val) {
+      this.busOrder=val
+      this.$emit("busOrderFun",this.busOrder)
     },
   },
-};
+}
 </script>
 
 <style lang="scss">
 .busListFormPage {
+  width: 100%;
+  max-width: 840px;
   position: fixed;
   top: 4px;
   left: 4px;
   z-index: 10;
   background: white;
   border-radius: 8px;
-
+  .el-tabs__content{
+    padding: 0;
+  }
   &__table {
+    .el-table__header-wrapper .el-checkbox {
+      display: none;
+    }
     .el-table__row {
       cursor: pointer;
     }
