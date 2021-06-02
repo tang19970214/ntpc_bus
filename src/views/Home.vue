dec5b69883f61c21cc7248c8bbd4c10709792e60
george <tang19970214@gmail.com>
2021-06-02 15:26:49 +0800

    upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="homePage">
-    <BusListForm
-      :busList="busList"
-      @getBusInfo="getBusInfo"
-      @initMap="initMap()"
-      @getBusOrder="getBusOrder"
-    />
+    <BusListForm :busList="busList" @getBusInfo="getBusInfo" @initMap="initMap()" @getBusOrder="getBusOrder" />
     <div id="map" ref="map"></div>
   </div>
 </template>
 
 <script>
-import BusListForm from "../components/BusListForm.vue"
+import BusListForm from "../components/BusListForm.vue";
 
 export default {
   name: "Home",
@@ -23,38 +18,31 @@ export default {
       busMarkList: [],
       map: document.getElementById("map"),
       test: [],
-    }
+    };
   },
   methods: {
     //勾選巴士後更新 busMarkList
     getBusOrder(order) {
       if (order.length > 0) {
-        // this.busMarkList = order // order is array
-
-        let orderArray = []
-        order.forEach((orderItem) => {
-          let markArray = this.busMarkList.filter((res) => {
-            // console.log("res", res)
-            return res.BusID == orderItem.carId
-          })
-          console.log("markArray", markArray)
-          // orderArray = markArray
-          // return orderArray
-        })
-        // console.log("orderArray", orderArray)
-
-        this.setMaker()
+        // 取得所選車輛id
+        let getBusIds = order.map((res) => res.carId);
+        let getBusLocal = this.busMarkList.filter((resp) =>
+          getBusIds.includes(Number(resp.BusID))
+        );
+        this.initMap(getBusLocal);
       } else {
-        this.busMarkList = this.$store.state.busMarkList
+        this.initMap(this.busMarkList);
+        return;
+        this.busMarkList = this.$store.state.busMarkList;
       }
     },
 
     //定位巴士，顯示 dialog
     getBusInfo(row) {
-      let getMark = this.busMarkList.filter((res) => res.BusID == row.carId)[0]
-      this.map.panTo({ lat: Number(getMark.Lat), lng: Number(getMark.Lon) })
+      let getMark = this.busMarkList.filter((res) => res.BusID == row.carId)[0];
+      this.map.panTo({ lat: Number(getMark.Lat), lng: Number(getMark.Lon) });
     },
-    initMap() {
+    initMap(getBusLocal) {
       this.map = new google.maps.Map(document.getElementById("map"), {
         center: {
           //原始中心點
@@ -76,15 +64,24 @@ export default {
         // terrain 顯示基於地形信息的物理地圖。
         mapTypeId: "roadmap",
         // 需要再前往修改，https://mapstyle.withgoogle.com/
-      })
-      let marker = this.setMaker()
+      });
+      let marker = this.setMaker(getBusLocal);
     },
-    setMaker() {
+    setMaker(getBusLocal) {
+      // 將巴士及小車合併成一個array
+      let allList = [];
+      if (!!getBusLocal) {
+        allList = getBusLocal.concat(this.cardList);
+      } else {
+        allList = this.busMarkList.concat(this.cardList);
+      }
+      console.log(allList);
+
       // busMarkList & cardList 合併跑回圈
-      let newList = [...this.busMarkList, ...this.cardList]
+      // let newList = [...this.busMarkList, ...this.cardList];
       // console.log("newList", newList)
-      newList.forEach((location) => {
-        const busInfo = []
+      allList.forEach((location) => {
+        // const busInfo = [];
         // console.log("location", location)
         // let icon = ""
         // if (location.carId) {
@@ -98,14 +95,16 @@ export default {
             lat: Number(location.Lat),
             lng: Number(location.Lon),
           },
-          icon: require("@/assets/images/busIcon@1x.png"),
+          icon: location.BusID
+            ? require("@/assets/images/busIcon@1x.png")
+            : require("@/assets/images/carIcon@1x.png"),
           map: this.map,
-        })
+        });
         // 透過 InfoWindow 物件建構子建立新訊息視窗
-      })
-      return
-      this.getDialogInfo(busInfo)
-      this.openDialogInfo(marker)
+      });
+      return;
+      this.getDialogInfo(busInfo);
+      this.openDialogInfo(marker);
     },
     getDialogInfo(busInfo) {
       const infowindow = new google.maps.InfoWindow({
@@ -132,34 +131,34 @@ export default {
         `,
         // 設定訊息視窗最大寬度
         maxWidth: 200,
-      })
+      });
     },
     openDialogInfo(marker) {
       // 在地標上監聽點擊事件
       marker.addListener("click", () => {
         // 指定在哪個地圖和地標上開啟訊息視窗
-        infowindow.open(this.map, marker)
-      })
+        infowindow.open(this.map, marker);
+      });
     },
-    getCarInfo() {
-      this.axios
+    async getCarInfo() {
+      await this.axios
         .get("http://care.1966.org.tw/api/api/DriverInfo/DeviceGpsViewModel")
         .then((res) => {
-          this.cardList = res.data.response
+          this.cardList = res.data.response;
         })
         .catch((error) => {
-          console.log(error)
-        })
+          console.log(error);
+        });
     },
   },
-  mounted() {
-    this.busList = this.$store.state.busList
-    this.busMarkList = this.$store.state.busMarkList
-    this.getCarInfo()
-    this.initMap()
-    this.setMaker()
+  async mounted() {
+    this.busList = this.$store.state.busList;
+    this.busMarkList = this.$store.state.busMarkList;
+    await this.getCarInfo();
+    this.initMap();
+    // this.setMaker();
   },
-}
+};
 </script>
 
 <style lang="scss">
