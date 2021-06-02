673810789558f1f30cb8e3890f573eb46ea5232e
coderken <jk0910948659@gmail.com>
2021-06-02 10:53:04 +0800

    delete none function

--- src/api/apis.js
+++ src/api/apis.js
@@ -1,7 +1,5 @@
-import {
-  Loading
-} from "element-ui";
-import req from "./https";
+import { Loading } from "element-ui"
+import req from "./https"
 
 // 全域 api 掛載於 Vue.prototype
 // 使用方式 this.$api
@@ -9,8 +7,8 @@ import req from "./https";
 const api = {
   /* bus */
   DisasterEvent(params) {
-    return req("post", "OMS/jsp/DisasterEvent/DEAJAX.jsp", params);
+    return req("post", "OMS/jsp/DisasterEvent/DEAJAX.jsp", params)
   },
-};
+}
 
-export default api;
+export default api
