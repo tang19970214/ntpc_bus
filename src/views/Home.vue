bde35f424f7cd9c192753e444dad10897e2c9cd0
coderken <jk0910948659@gmail.com>
2021-06-09 19:35:36 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,52 +25,16 @@ export default {
       map: document.getElementById("map"),
       // 存放目前開啟的訊息視窗
       infowindow: null,
-      latMarker: 23.47316642202988,
-      lonMarker: 120.29256820678711,
+      latMarker: 23.473166,
+      lonMarker: 120.292568,
       Point1: null,
       Point2: null,
+      Point3: null,
       duration: null,
+      tootTipTime: null,
     }
   },
   methods: {
-    getDistanceTime() {
-      let map
-      let directionsService = new google.maps.DirectionsService()
-      let directionsRenderer = new google.maps.DirectionsRenderer()
-
-      directionsRenderer.setMap(map)
-
-      const route = {
-        origin: this.Point1,
-        destination: this.Point2,
-        travelMode: "DRIVING",
-      }
-      directionsService.route(route, (response, status) => {
-        this.duration = response.routes[0].legs[0].duration.text
-        // console.log("this.duration", this.duration)
-        return
-        if (status !== "OK") {
-          window.alert("Directions request failed due to " + status)
-          return
-        } else {
-          directionsRenderer.setDirections(response) // Add route to the map
-          let directionsData = response.routes[0].legs[0] // Get data about the mapped route
-          // console.log("directionsData", directionsData)
-          if (!directionsData) {
-            window.alert("Directions request failed")
-            return
-          } else {
-            document.getElementById("msg").innerHTML +=
-              // document.getElementsByClassName("markerPopover").innerHTML +=
-              "距離 ： " +
-              directionsData.distance.text +
-              " (" +
-              directionsData.duration.text +
-              ")."
-          }
-        }
-      })
-    },
     //勾選巴士後更新 busMarkList
     getBusOrder(order) {
       if (order.length > 0) {
@@ -86,7 +50,7 @@ export default {
     },
 
     //定位巴士，顯示 dialog
-    getBusInfo(row) {
+    async getBusInfo(row) {
       let getMark = this.busMarkList.filter((res) => res.BusID == row.carId)[0]
       this.map.panTo({
         lat: Number(getMark.Lat),
@@ -103,16 +67,11 @@ export default {
           : require("@/assets/images/carIcon@1x.png"),
         map: this.map,
       })
-      // Point1 自定點位（目前是寫死狀態）
       this.Point1 = { lat: Number(this.latMarker), lng: Number(this.lonMarker) }
-      // Point2 點選巴士的點位
       this.Point2 = { lat: Number(getMark.Lat), lng: Number(getMark.Lon) }
-      this.getDistanceTime(this.Point1, this.Point2)
-      let totalTime = this.duration
-      // console.log("totalTime", totalTime)
-      // 透過 InfoWindow 物件建構子建立新訊息視窗
+      await this.getDistanceTime(this.Point1, this.Point2)
+      let getBusTime = this.duration
       const infowindow = new google.maps.InfoWindow({
-        // 設定想要顯示的內容
         content:
           `
           <div class="markerPopover">
@@ -132,11 +91,10 @@ export default {
           row.carNo +
           `</p>
            <p>預估時間：` +
-          totalTime +
+          getBusTime +
           `</p>
           </div>
         `,
-        // 設定訊息視窗最大寬度
         maxWidth: 200,
       })
       if (this.infowindow) {
@@ -167,7 +125,7 @@ export default {
       this.setMarker(getBusLocal)
     },
     // 建立地標
-    setMarker(getBusLocal) {
+    async setMarker(getBusLocal) {
       // 將巴士及小車合併成一個array
       let allList = []
       if (!!getBusLocal) {
@@ -175,12 +133,29 @@ export default {
       } else {
         allList = this.busMarkList.concat(this.cardList)
       }
+
       // 整理出所有車牌
       let getBusInfos = allList.map((res) => res.BusID)
-      allList.forEach((location, idx) => {
+      await allList.forEach((location, idx) => {
         let getInfos = this.busList.filter(
           (res) => getBusInfos.includes(res.carId) || {}
         )
+
+        this.Point1 = {
+          lat: Number(this.latMarker),
+          lng: Number(this.lonMarker),
+        }
+        this.Point2 = {
+          lat: Number(location.Lat),
+          lng: Number(location.Lon),
+        }
+        setTimeout(() => {
+          this.getDistanceTime(this.Point1, this.Point2)
+        }, 1500)
+
+        // let tootTipTime = this.duration
+        // console.log("tootTipTime", tootTipTime)
+
         const marker = new google.maps.Marker({
           //原始中心點
           position: {
@@ -192,7 +167,6 @@ export default {
             : require("@/assets/images/carIcon@1x.png"),
           map: this.map,
         })
-        // console.log("marker", marker)
         // 透過 InfoWindow 物件建構子建立新訊息視窗
         const infowindow = new google.maps.InfoWindow({
           // 設定想要顯示的內容
@@ -214,9 +188,7 @@ export default {
             <p>車輛：` +
             getInfos[idx]?.carNo +
             `</p>
-            <p>車輛：` +
-            getInfos[idx]?.carNo +
-            `</p>
+
           </div>
         `,
           // 設定訊息視窗最大寬度
@@ -234,6 +206,39 @@ export default {
             this.infowindow = infowindow
           }
         })
+      })
+    },
+
+    async getDistanceTime(Point1, Point2) {
+      // console.log(Point1, Point2)
+      // return
+      let directionsService = new google.maps.DirectionsService()
+      const points = {
+        origin: Point1, // 自訂點位
+        destination: Point2, //點選巴士點位
+        travelMode: "DRIVING", // 交通方式（開車)
+      }
+      // console.log();
+
+      directionsService.route(points, (response, status) => {
+        if (status == "OK") {
+          // setTimeout(() => {
+          this.duration = response.routes[0].legs[0].duration.text
+          // }, 500)
+          return
+        } else if (status == "OVER_QUERY_LIMIT") {
+          // } else if (status !== "OK") {
+          console.log("status", status)
+          // return
+          // setTimeout(() => {
+          // this.getDistanceTime(Point1, Point2)
+          // }, 1500)
+        } else {
+          console.log("Route: " + status)
+        }
+        return
+        this.duration = response.routes[0].legs[0].duration.text
+        // console.log("this.duration", this.duration)
       })
     },
     async getCarInfo() {
